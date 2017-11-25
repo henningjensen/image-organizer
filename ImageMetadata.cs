05e1c55c8bf16e72e1a07788a33526f1d875200c
Henning Jensen <henning.jensen@gmail.com>
2017-11-25 21:00:24 +0100

    Initial project files

---
+++ ImageMetadata.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using ExifLib;
+using MetadataExtractor;
+
+class ImageMetadata
+{
+    FileInfo _filePath;
+    ExifReader _exifReader;
+
+    public ImageMetadata(string filepath)
+    {
+        _filePath = new FileInfo(filepath);
+        _exifReader = new ExifReader(_filePath.FullName);
+    }
+
+    public string Caption() {
+        IEnumerable<MetadataExtractor.Directory> directories = ImageMetadataReader.ReadMetadata(_filePath.FullName);
+        var subIfdDirectory = directories.OfType<MetadataExtractor.Formats.Exif.ExifSubIfdDirectory>().FirstOrDefault();
+        var userComment = subIfdDirectory?.GetDescription(MetadataExtractor.Formats.Exif.ExifDirectoryBase.TagUserComment);
+        return userComment;
+    }
+    
+    public string DirectoryName() {
+        return _filePath.Directory.Name;
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using MetadataExtractor;
+using System.Linq;
+using MetadataExtractor.Formats.Exif;
+
+namespace image_organizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var p = new FileInfo(args[0]).FullName;
+
+            var metadata = new ImageMetadata(args[0]);
+
+            Console.WriteLine("Caption: " + metadata.Caption());
+            Console.WriteLine("Directory: " + metadata.DirectoryName());
+            
+/*
+            IEnumerable<MetadataExtractor.Directory> directories = ImageMetadataReader.ReadMetadata(p);
+            var subIfdDirectory = directories.OfType<MetadataExtractor.Formats.Iptc.IptcDirectory>().FirstOrDefault();
+            var dateTime = subIfdDirectory?.GetDescription(MetadataExtractor.Formats.Iptc.IptcDirectory.TagCategory);
+            Console.WriteLine(dateTime);
+
+            var exifReader = new ExifLib.ExifReader(p);
+            
+            //Double[] latitude;
+            exifReader.GetTagValue<Double[]>(ExifLib.ExifTags.GPSLatitude, out Double[] latitude);
+
+            // Double[] longitude;
+            exifReader.GetTagValue<Double[]>(ExifLib.ExifTags.GPSLongitude, out Double[] longitude);
+
+            
+
+            Console.WriteLine(p);
+
+            if (latitude != null)
+                Console.WriteLine($"Lat: {latitude[0]},{latitude[1]}");
+            if (longitude != null)
+                Console.WriteLine($"Lon: {longitude[0]},{longitude[1]}");
+
+                 */
+
+        }
+
+
+
+    }
+}
